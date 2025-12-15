e9d46616d3705683a449aea13a351e1b744578ae
Khalid <nwakala730@gmail.com>
2025-12-15 09:21:48 +0300

    basic get hash function

---
+++ hash_map.py
@@ -0,0 +1,13 @@
+class HashTable: #hash map is the same as hash table
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+        
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+            
+        return h % self.MAX
+    
+    
