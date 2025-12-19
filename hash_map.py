bc0caeca0f51943f4821299816f8ec29948b832e
Khalid <nwakala730@gmail.com>
2025-12-19 09:03:55 +0300

    add collision detection

--- hash_map.py
+++ hash_map.py
@@ -1,7 +1,7 @@
 class HashTable: #hash map is the same as hash table
     def __init__(self):
-        self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.MAX = 10
+        self.arr = [[] for i in range(self.MAX)]
         
     def get_hash(self, key):
         h = 0
@@ -12,19 +12,44 @@ class HashTable: #hash map is the same as hash table
     
     def __setitem__(self, key, val):
         index = self.get_hash(key)
-        self.arr[index] = val
+        found = False
+        
+        for idx, element in enumerate(self.arr[index]):
+            if len(element) == 2 and element[0]==key:                
+                self.arr[index][idx] = (key,val)
+                found = True
+                break
+        if not found:
+            self.arr[index].append((key,val))
         
     def __getitem__(self, key):
         index = self.get_hash(key)
-        return self.arr[index]
-    
+        
+        for element in self.arr[index]:
+            if element[0] == key:
+                return element[1]
+            
+    def __delitem__(self, key):
+        index = self.get_hash(key)
+        
+        for idx, element in enumerate(self.arr[index]):
+            if element[0] == key:
+                del self.arr[index][idx]
+                
+                
 dic = HashTable()
 # dic.add("march 6")
 # dic.add("march 4")
 # dic.add("march 8")
-dic["march 6"] = "max amini"
+dic["march 6"] = 120
+dic["march 8"] = 32
+dic["march 9"] = 90
+dic["march 17"] = 432
+# print(dic.arr)
 print(dic["march 6"])
 
-# print(dic.arr)
+del dic["march 17"]
+
+print(dic.arr)
     
     
