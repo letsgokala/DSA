026acdfc93f6bfd9734dd55915575626c485c41f
Khalid <nwakala730@gmail.com>
2025-12-21 12:35:10 +0300

    food queue challenge

--- stack.py
+++ stack.py
@@ -1,5 +1,6 @@
 from collections import deque
 from threading import Thread
+import time
 
 class Stack:
     def __init__(self):
@@ -20,4 +21,57 @@ class Stack:
     def size(self):
         return len(self.container)
     
+class Queue:
+    def __init__(self):
+        self.buffer = deque()
+        
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
+        
+    def dequeue(self):
+        if len(self.buffer) == 0:
+            print("Queue is empty")
+            return
+        
+        return self.buffer.pop()    
     
+    def is_empty(self):
+        return len(self.buffer) == 0
+    
+    def size(self):
+        return len(self.buffer)
+    
+food_order_queue = Queue()
+
+def place_orders(orders):
+    for order in orders:
+        print("placing order for:" , order)
+        food_order_queue.enqueue(order)
+        time.sleep(0.5)
+        
+def serve_orders():
+    time.sleep(1)
+    while food_order_queue.size() > 0:
+        order = food_order_queue.dequeue()
+        print("Now serving: ", order)
+        time.sleep(2)
+    
+orders = ['pizza','samosa','pasta','biryani','burger']
+
+thread1 = Thread(target=place_orders, args=(orders,))
+thread2 = Thread(target=serve_orders, )
+
+
+thread1.start()
+thread2.start()
+
+thread1.join()
+thread2.join()
+
+print("done")
+    
+# elts = deque()
+# elts.extend([1,2,3,4,5,6,7,8])
+# print(elts)
+# elts.rotate(1)
+# print(elts)
