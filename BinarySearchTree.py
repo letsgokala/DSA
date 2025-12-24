e4725ff81dd9a26241955d717df903ca45b266ca
Khalid <nwakala730@gmail.com>
2025-12-24 11:31:38 +0300

    add dfs search techniques

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -20,13 +20,49 @@ class BinarySearchTree:
                 self.right = BinarySearchTree(data)
                 
     def in_order_traversal(self):
+        """
+    Traverses the tree in the order: Left -> Root -> Right.
+    In a Binary Search Tree, this visits nodes in ascending (sorted) order.
+    """
         if self.left:
             self.left.in_order_traversal()
         print(self.data)
         if self.right:
             self.right.in_order_traversal()
             
+    def pre_order_traversal(self):
+        """
+        Traverses the tree in the order: Root -> Left -> Right.
+        Useful for creating a copy of the tree or representing a structural hierarchy.
+        """
+        print(self.data)
+        if self.left:
+            self.left.pre_order_traversal()
+        if self.right:
+            self.right.pre_order_traversal()
+
+    
+    def post_order_traversal(self):
+        """
+        Traverses the tree in the order: Left -> Right -> Root.
+        Commonly used for deleting nodes or evaluating mathematical expression trees.
+        """                
+        if self.left:
+            self.left.post_order_traversal()
+        if self.right:
+            self.right.post_order_traversal()
+        print(self.data)
+            
     def search(self, val):
+        """
+        Recursively searches for a value in the tree.
+
+        Args:
+            val: The value to look for in the tree.
+
+        Returns:
+            bool: True if the value is found, False otherwise.
+        """
         if self.data == val:
             return True
         
@@ -44,6 +80,15 @@ class BinarySearchTree:
             
             
 def build_tree(elements):
+    '''
+    builds our tree
+    
+    Args: 
+        a list that contains the elements of the tree
+    
+    returns:
+        Binary search tree
+    '''
     root = BinarySearchTree(elements[0])
     for i in range(1, len(elements)):
         root.add_child(elements[i])
@@ -53,7 +98,12 @@ def build_tree(elements):
             
 numbers = [17,32,65,34,1,56,2,51,54,326,23]
 bst = build_tree(elements=numbers)
+print(f"==================IN ORDER TRAVERSAL================")
 bst.in_order_traversal()
+print(f"==================PRE ORDER TRAVERSAL================")
+bst.pre_order_traversal()
+print(f"==================POST ORDER TRAVERSAL================")
+bst.post_order_traversal()
 print(bst.search(326))
 
     
